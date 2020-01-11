24a253ffbd18425f8088967a1fdedf59eb21817c
doxas <eizanco@gmail.com>
2020-01-11 19:28:54 +0900

    small fix

--- sample/0001/script.js
+++ sample/0001/script.js
@@ -1,12 +1,12 @@
 
 // JavaScript での関数の定義
-function loaded() {
+function loaded(){
     // const キーワードでの変数宣言と、セレクタによる要素の参照
     const out = document.querySelector('#out');
     // 参照先の要素にテキストを設定
     out.textContent = '😈😈😈';
 }
 
-// ウェブページのコンテンツがロードした時点で自動的に関数を実行
+// ウェブページのコンテンツがロード完了した時点で自動的に関数を実行
 window.addEventListener('DOMContentLoaded', loaded, false);
 
