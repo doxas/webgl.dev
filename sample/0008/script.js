1a9b1b21dde4b88844407594114e21e5fbc6610d
doxas <eizanco@gmail.com>
2020-01-14 21:21:59 +0900

    fix sample script

--- sample/0008/script.js
+++ sample/0008/script.js
@@ -1,30 +1,20 @@
 
-import {Clock} from './clock.js';
+// 同階層にある CanvasUtility.js をインポートする
+import {CanvasUtility} from './CanvasUtility.js';
 
+// DOM のロードが完了した際の処理を登録
 window.addEventListener('DOMContentLoaded', () => {
-    const clock = new Clock();
-    const intervalTime = 1000;
-    const out = document.querySelector('#out');
+    // ラッパー DOM の参照を取得
+    const wrapper = document.querySelector('#wrapper');
 
-    clock.timer(intervalTime)
-    .then((response) => {
-        const msg = `${response}ミリ秒が経過！`;
-        console.log(msg);
-        out.textContent = msg;
-    })
-    .catch((err) => {
-        console.warn(err);
+    // CanvasUtility クラスのインスタンスを生成する
+    const canvasUtil = new CanvasUtility({
+        appendTo: wrapper, // アペンドする DOM
+        width: 512,        // 幅
+        height: 512,       // 高さ
     });
 
-    // キー押下に対するリスナーを設定する例
-    window.addEventListener('keydown', (evt) => {
-        // ここでの引数は KeyboardEvent オブジェクト
-        out.textContent = evt.key;
-    }, false);
-    // マウス移動に対するリスナーを設定する例
-    out.addEventListener('mousemove', (evt) => {
-        // ここでの引数は MouseEvent オブジェクト
-        out.textContent = `x: ${evt.clientX}, y: ${evt.clientY}`;
-    }, false);
+    // クリアするメソッドを呼ぶ
+    canvasUtil.clear();
 }, false);
 
