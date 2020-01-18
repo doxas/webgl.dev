ee0bacedab1a55a024abc089bc6c264380184b0f
doxas <eizanco@gmail.com>
2020-01-18 21:54:04 +0900

    fix whitespace

--- sample/0017/script.js
+++ sample/0017/script.js
@@ -4,15 +4,15 @@ const os = require('os');
 
 // os モジュールを使って各種情報を取得
 const info = {
-    arch: os.arch(),
-    cpus: os.cpus(),
-    totalmem: os.totalmem(),
-    freemem: os.freemem(),
-    platform: os.platform(),
-    release: os.release(),
+    arch:              os.arch(),
+    cpus:              os.cpus(),
+    totalmem:          os.totalmem(),
+    freemem:           os.freemem(),
+    platform:          os.platform(),
+    release:           os.release(),
     networkInterfaces: os.networkInterfaces(),
-    homedir: os.homedir(),
-    tmpdir: os.tmpdir(),
+    homedir:           os.homedir(),
+    tmpdir:            os.tmpdir(),
 };
 
 // そのままでは見づらいので整形した JSON で出力
