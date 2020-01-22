5e1a4659678e423690005f13e8a9e338066fe6ad
doxas <eizanco@gmail.com>
2020-01-22 17:35:51 +0900

    add new sample

---
+++ sample/0022/MathUtility.js
@@ -0,0 +1,217 @@
+
+export class MathUtility {
+    static get Vec2(){
+        return Vec2;
+    }
+}
+
+class Vec2 {
+    // static method
+    static calcLength(x, y){
+        return Math.sqrt(x * x + y + y);
+    }
+    /**
+     * @constructor
+     * @param {number} x - X 座標
+     * @param {number} y - Y 座標
+     */
+    constructor(x, y){
+        this.x = 0.0;
+        this.y = 0.0;
+        this.set(x, y);
+    }
+    /**
+     * 値を設定する
+     * @param {number} x - X 座標
+     * @param {number} y - Y 座標
+     * @return {Vec2} 値設定後の自身のインスタンス
+     */
+    set(x, y){
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+    /**
+     * 自身の符号を反転する
+     * @return {Vec2} 符号反転後の自身のインスタンス
+     */
+    negate(){
+        this.x = -this.x;
+        this.y = -this.y;
+        return this;
+    }
+    /**
+     * 自身を単位化する
+     * @return {Vec2} 単位化後の自身のインスタンス
+     */
+    normalize(){
+        const l = this.length;
+        this.x /= l;
+        this.y /= l;
+        return this;
+    }
+    /**
+     * 自身の値を複製した新しい Vec2 インスタンスを返す
+     * @return {Vec2} 自身と同じ値を持つ Vec2 インスタンス
+     */
+    clone(){
+        return new Vec2(this.x, this.y);
+    }
+    /**
+     * 与えられた Vec2 インスタンスと自身との距離を返す
+     * @param {Vec2} v - 距離を測る対象となる Vec2 インスタンス
+     * @return {number} 引数に与えられた Vec2 インスタンスとの距離
+     */
+    distance(v){
+        if(v instanceof Vec2){
+            throw new Error('Vec2.distance: invalid argument');
+        }
+        const x = this.x - v.x;
+        const y = this.y - v.y;
+        return Vec2.calcLength(x, y);
+    }
+    /**
+     * 与えられた Vec2 インスタンスと自身の要素とを加算する
+     * @param {Vec2} v - 加算の対象となる Vec2 インスタンス
+     * @return {Vec2} 加算後の自身のインスタンス
+     */
+    add(v){
+        if(v instanceof Vec2){
+            throw new Error('Vec2.add: invalid argument');
+        }
+        this.x += v.x;
+        this.y += v.y;
+        return this;
+    }
+    /**
+     * 与えられたスカラーの値と自身の要素とを加算する
+     * @param {number} s - 加算の対象となるスカラーの値
+     * @return {Vec2} 加算後の自身のインスタンス
+     */
+    addScalar(s){
+        this.x += s;
+        this.y += s;
+        return this;
+    }
+    /**
+     * 与えられた Vec2 インスタンスと自身の要素とを減算する
+     * @param {Vec2} v - 減算の対象となる Vec2 インスタンス
+     * @return {Vec2} 減算後の自身のインスタンス
+     */
+    sub(v){
+        if(v instanceof Vec2){
+            throw new Error('Vec2.sub: invalid argument');
+        }
+        this.x -= v.x;
+        this.y -= v.y;
+        return this;
+    }
+    /**
+     * 与えられたスカラーの値と自身の要素とを減算する
+     * @param {number} s - 減算の対象となるスカラーの値
+     * @return {Vec2} 減算後の自身のインスタンス
+     */
+    subScalar(s){
+        this.x -= s;
+        this.y -= s;
+        return this;
+    }
+    /**
+     * 与えられた Vec2 インスタンスと自身の要素とを乗算する
+     * @param {Vec2} v - 乗算の対象となる Vec2 インスタンス
+     * @return {Vec2} 乗算後の自身のインスタンス
+     */
+    multiply(v){
+        if(v instanceof Vec2){
+            throw new Error('Vec2.multiply: invalid argument');
+        }
+        this.x *= v.x;
+        this.y *= v.y;
+        return this;
+    }
+    /**
+     * 与えられたスカラーの値と自身の要素とを乗算する
+     * @param {number} s - 乗算の対象となるスカラーの値
+     * @return {Vec2} 乗算後の自身のインスタンス
+     */
+    multiplyScalar(s){
+        this.x *= s;
+        this.y *= s;
+        return this;
+    }
+    /**
+     * 与えられた Vec2 インスタンスと自身の要素とを除算する
+     * @param {Vec2} v - 除算の対象となる Vec2 インスタンス
+     * @return {Vec2} 除算後の自身のインスタンス
+     */
+    divide(v){
+        if(v instanceof Vec2){
+            throw new Error('Vec2.divide: invalid argument');
+        }
+        this.x /= v.x;
+        this.y /= v.y;
+        return this;
+    }
+    /**
+     * 与えられたスカラーの値と自身の要素とを除算する
+     * @param {number} s - 除算の対象となるスカラーの値
+     * @return {Vec2} 除算後の自身のインスタンス
+     */
+    divideScalar(s){
+        this.x /= s;
+        this.y /= s;
+        return this;
+    }
+    /**
+     * 与えられた Vec2 インスタンスと自身との内積の結果を返す
+     * @param {Vec2} v - 内積の対象となる Vec2 インスタンス
+     * @return {number} 内積の結果
+     */
+    dot(v){
+        return this.x * v.x + this.y * v.y;
+    }
+    /**
+     * 与えられた Vec2 インスタンスと自身との外積の結果を返す
+     * @param {Vec2} v - 外積の対象となる Vec2 インスタンス
+     * @return {number} 外積の結果
+     */
+    cross(v){
+        return this.x * v.y - this.y * v.x;
+    }
+
+    // getter -----------------------------------------------------------------
+    /**
+     * 自身のインスタンスの長さ
+     * @type {number}
+     */
+    get length(){
+        return Vec2.calcLength(this.x, this.y);
+    }
+    /**
+     * 自身のインスタンスを配列化したもの
+     * @type {Array.<number>}
+     */
+    get array(){
+        return [this.x, this.y];
+    }
+
+    // setter -----------------------------------------------------------------
+    /**
+     * 自身のインスタンスに配列から値を設定する
+     * @type {Array.<number>}
+     */
+    set array(values){
+        if(
+            Array.isArray(values) !== true &&
+            values instanceof Object.getPrototypeOf(Int8Array) !== true
+        ){
+            throw new Error('Vec2.array[set]: invalid type');
+        }
+        if(values.length < 2){
+            throw new Error('Vec2.array[set]: invalid length');
+        }
+        this.x = values[0];
+        this.y = values[1];
+    }
+}
+
---
+++ sample/0022/script.js
@@ -0,0 +1,134 @@
+
+import {CanvasUtility} from './CanvasUtility.js';
+import {MathUtility} from './MathUtility.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+    // Canvas の幅と高さは定数化しておく
+    const WIDTH = 512;
+    const HEIGHT = 512;
+    // グリッドの細かさは定数化しておく
+    const GRID_COUNT = 10;
+
+    // ラッパー DOM の参照を取得
+    const wrapper = document.querySelector('#wrapper');
+
+    // CanvasUtility クラスのインスタンスを生成する
+    const canvasUtil = new CanvasUtility({
+        appendTo: wrapper,
+        width: WIDTH,
+        height: HEIGHT,
+    });
+
+    // Shift キー押下で角度を固定できるようにするための処理
+    let latestAngle = 0;
+
+    // Canvas 上でマウスカーソルが動いた際に描画を行う
+    canvasUtil.canvas.addEventListener('mousemove', mouseMove, false);
+
+    // 見た目のわかりやすさのためにラインの太さを２倍にしておく
+    canvasUtil.lineWidth = 2;
+
+    // 初期状態としてグリッドを描画しておく
+    drawGrid();
+
+    function mouseMove(evt){
+        // クリアするメソッドを呼ぶ
+        canvasUtil.clear();
+        // マウスカーソルの Canvas 上での位置を計算するために……
+        // Canvas のスクリーン上での位置の情報を取得
+        const bound = canvasUtil.canvas.getBoundingClientRect();
+        // 座標は MouseEvent.clientX から Canvas の横位置を引いて求める
+        const x = evt.clientX - bound.x;
+        const y = evt.clientY - bound.y;
+
+        // グリッドの再描画を行う
+        drawGrid();
+        // ベクトルを表すラインを描画する
+        drawLine(x, y);
+    }
+
+    /**
+     * グリッドを描画する
+     */
+    function drawGrid(){
+        // Canvas の中心を求めるため、幅と高さの半分の値を求める
+        const centerX = WIDTH / 2;
+        const centerY = HEIGHT / 2;
+        // 薄いグリッドをループ処理で一気に描画する
+        const gridWidth = (WIDTH / 2) / GRID_COUNT;
+        const gridHeight = (HEIGHT / 2) / GRID_COUNT;
+        for(let i = 0; i < GRID_COUNT; ++i){
+            const intervalX = gridWidth * (i + 1);
+            const intervalY = gridHeight * (i + 1);
+            canvasUtil.strokeLine(centerX + intervalX, 0, centerX + intervalX, HEIGHT, 'silver');
+            canvasUtil.strokeLine(centerX - intervalX, 0, centerX - intervalX, HEIGHT, 'silver');
+            canvasUtil.strokeLine(0, centerY + intervalY, WIDTH, centerY + intervalY, 'silver');
+            canvasUtil.strokeLine(0, centerY - intervalY, WIDTH, centerY - intervalY, 'silver');
+        }
+        // 十字ラインをグレーで描画する
+        canvasUtil.strokeLine(0, centerY, WIDTH, centerY, 'gray');
+        canvasUtil.strokeLine(centerX, 0, centerX, HEIGHT, 'gray');
+    }
+
+    /**
+     * ベクトルを表す原点から伸びるラインを描画する
+     * @param {number} x - ベクトルの終点の X 座標
+     * @param {number} y - ベクトルの終点の Y 座標
+     */
+    function drawLine(x, y){
+        // Canvas の中心を求めるため、幅と高さの半分の値を求める
+        const centerX = WIDTH / 2;
+        const centerY = HEIGHT / 2;
+
+        // グリッドの幅に合わせた単位に変換する
+        const unit = 1.0 / GRID_COUNT * 2;         // グリッドの幅
+        const gridX = (x - centerX) / centerX * 2; // グリッド幅に応じた X の値
+        const gridY = (y - centerY) / centerY * 2; // グリッド幅に応じた Y の値
+        // ベクトルの長さを計算する
+        const V = new MathUtility.Vec2(gridX, gridY);
+        console.log(V.length);
+        const length = Math.sqrt(gridX * gridX + gridY * gridY);
+
+        // わかりやすさのために線を太くする
+        canvasUtil.lineWidth = 6;
+
+        // X 軸に水平なベクトルをピンクのラインで描画する
+        canvasUtil.strokeLine(centerX, centerY, centerX + centerX / 2, centerY, 'deeppink');
+        // マウスカーソル位置へのベクトルを単位化してから青のラインで描画する
+        const nx = gridX / length; // X を単位化
+        const ny = gridY / length; // Y を単位化
+        const cx = centerX + nx * centerX / 2; // グリッドに合わせた量に変換
+        const cy = centerY + ny * centerY / 2; // グリッドに合わせた量に変換
+        canvasUtil.strokeLine(centerX, centerY, cx, cy, 'deepskyblue');
+
+        // 青のベクトルは以下のように X 軸に水平
+        // ※ X が 1.0 で Y が 0.0 なので単位ベクトル
+        const blueX = 1.0;
+        const blueY = 0.0;
+
+        // ピンクと青のベクトルの内積を求める
+        const dotProduct = nx * blueX + ny * blueY;
+        // 内積の結果をオレンジの棒グラフで描画
+        const dHorizontal = centerX + dotProduct * (WIDTH / 4);
+        const dVertical = HEIGHT - 100;
+        canvasUtil.strokeLine(centerX, dVertical, dHorizontal, dVertical, 'darkorange');
+
+        // ピンクと青のベクトルの外積を求める
+        const crossProduct = nx * blueY - ny * blueX;
+        // 外積の結果を緑の棒グラフで描画
+        const cHorizontal = 100;
+        const cVertical = centerY - crossProduct * (HEIGHT / 4);
+        canvasUtil.strokeLine(cHorizontal, centerY, cHorizontal, cVertical, 'green');
+
+        // 線の太さを元に戻す
+        canvasUtil.lineWidth = 2;
+
+        // ログの出力
+        canvasUtil.fillText(`グリッドの幅: ${unit}`, 10, 20);
+        canvasUtil.fillText(`２つの単位ベクトル同士の内積と外積は……`, 10, 40);
+        canvasUtil.fillText(`dot: ${dotProduct}`, 10, 70, 'darkorange');
+        canvasUtil.fillText(`cross: ${crossProduct}`, 10, 100, 'green');
+    }
+
+}, false);
+
