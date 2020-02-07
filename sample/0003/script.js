918788ab219042e8cde4d13e8447681f19a38f89
doxas <eizanco@gmail.com>
2020-02-07 23:44:40 +0900

    add new sample

---
+++ sample/0003/script.js
@@ -0,0 +1,26 @@
+
+// 数値
+console.log(999);
+console.log(9.99);
+
+// 文字（クォーテーションはクォートと表記する場合もあります）
+console.log('JavaScript'); // シングルクォーテーション
+console.log("JavaScript"); // ダブルクォーテーション
+console.log(`JavaScript`); // バッククォーテーション
+
+// 真偽値
+console.log(true);
+console.log(false);
+
+// 未定義であるということを意味する
+console.log(undefined);
+
+// 値が存在しないということを意味する
+console.log(null);
+
+// 配列
+console.log([]);
+
+// オブジェクト
+console.log({});
+
