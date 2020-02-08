53bef2cd5a746470222ac1a742736e1a59a77262
doxas <eizanco@gmail.com>
2020-02-08 17:00:21 +0900

    improve sample

--- sample/0003/script.js
+++ sample/0003/script.js
@@ -1,7 +1,12 @@
 
 // 数値
-console.log(999);
-console.log(9.99);
+console.log(999);  // 整数
+console.log(9.99); // 浮動小数点
+console.log(.99);  // 浮動小数点（整数部の省略時は 0 として扱われる）
+console.log(9e9);  // 浮動小数点（指数表記）
+console.log(0b11); // ２進数
+console.log(0o11); // ８進数
+console.log(0x11); // １６進数
 
 // 文字（クォーテーションはクォートと表記する場合もあります）
 console.log('JavaScript'); // シングルクォーテーション
@@ -23,4 +28,7 @@ console.log([]);
 
 // オブジェクト
 console.log({});
+
+// 正規表現
+console.log(/regexp/);
 
